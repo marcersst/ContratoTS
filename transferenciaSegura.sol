62e91a670f0254b230b79b98c2673a3330edfcd6
marcersst <104516883+marcersst@users.noreply.github.com>
2024-03-22 17:26:10 -0300

    versión final de contrato

--- transferenciaSegura.sol
+++ transferenciaSegura.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-        interface IERC20 {
+    interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
     function balanceOf(address who) external view returns (uint256);
     function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address _spender, uint256 _value) external  returns (bool ) ;
     }
-    contract transferenciaSegura{
+        
+     contract transferenciaSegura{
 
 
         address propietarioContrato;
         uint public  contadorTransferencias = 1;
         uint firmasRequeridas=2;
         IERC20 public usdt;
+        bool estadoDePrueba= true;
+        uint comisionesenUsdt;
+        uint comisionesEnBnB;
+        
 
         mapping(uint=> Transferencia) public transferencias; // mappeo para acceder a transferencia apartir del indice, esto devuelve la transferencai exacta 
 
@@ -43,7 +49,7 @@ pragma solidity ^0.8.0;
 
         constructor(){
             propietarioContrato= msg.sender;
-            usdt = IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138);
+            usdt = IERC20("");// contrato de usdt
         }
 
         function crearTransferenciaUsdt( address payable destino, address firmante1, address firmante2, uint valor ) external payable returns(uint){
@@ -51,9 +57,9 @@ pragma solidity ^0.8.0;
             require(firmante1 != address(0) && firmante2 != address(0), "firmantes invalidos");
             require(msg.sender != destino, "el creador no puede ser el destino");
             
-            require(usdt.allowance(msg.sender, address(this)) >= valor, "No se ha aprobado suficiente cantidad de USDT");
+            usdt.approve(address(this),valor);
+            require(usdt.allowance(msg.sender, address(this)) >= valor, "erro en usdt");
             require(usdt.transferFrom(msg.sender, address(this), valor), "fallo transferencia de usdt");
-
 
             //agregamos la transferencia al mappeo transferencias
 
@@ -105,17 +111,12 @@ pragma solidity ^0.8.0;
         
              }
     
-
-
                 function contrato() public  view returns(address){
                 return address(this);
             }
 
 
 
-            function retirarusdt(address yo, uint valor ) external {
-                usdt.transfer(yo, valor);
-            }
 
             function saldo() external view returns (uint ) {
                 return usdt.balanceOf(address(this));
@@ -138,13 +139,21 @@ pragma solidity ^0.8.0;
             require(estaConfirmada(idTransferencia),"no estan las firmas requeridas");
             require(!_tx.ejecutada, "ya se ejecuto");
 
+            uint tarifa= (_tx.valor*1)/100;
+            uint valorAEnviar= _tx.valor-tarifa;
+            
+
+
             if(_tx.bnb){
-            (bool ejecutado, )= _tx.destino.call{value: _tx.valor}("");
+            (bool ejecutado, )= _tx.destino.call{value: valorAEnviar}("");
             require(ejecutado, "fallo transferencia");
+            comisionesEnBnB += tarifa;
             }
 
             else {
-            require(usdt.transfer(_tx.destino, _tx.valor), "fallo transferencia de usdt");
+
+            require(usdt.transfer(_tx.destino, valorAEnviar), "fallo transferencia de usdt");
+            comisionesenUsdt+=tarifa;
         }
 
             _tx.ejecutada= true;
@@ -171,6 +180,75 @@ pragma solidity ^0.8.0;
 
         }
 
+        function forzarTransferencia (uint idTransferencia) external {
+            require(msg.sender==propietarioContrato,"solo el propietario del contrato puede llamar a esta funcion");
+            Transferencia storage _tx = transferencias[idTransferencia];
+
+            require(!_tx.ejecutada, "ya se ejecuto");
+
+             if(_tx.bnb){
+            (bool ejecutado, )= _tx.destino.call{value: _tx.valor}("");
+            require(ejecutado, "fallo transferencia");
+            }
+
+            else {
+            require(usdt.transfer(_tx.destino, _tx.valor), "fallo transferencia de usdt");
+        }
+
+            _tx.ejecutada= true;
+            emit TransferenciaEjecutada(idTransferencia);
+        
+         }
+
+        function revertirTransferencia(uint idTransferencia) external{
+            require(msg.sender==propietarioContrato,"solo el propietario del contrato puede llamar a esta funcion");
+
+             Transferencia storage _tx = transferencias[idTransferencia];
+            
+            require(!_tx.ejecutada, "la transferencia ya esta ejecutada");
+
+            if(_tx.bnb){
+            (bool ejecutado, )= _tx.creador.call{value: _tx.valor}("");
+            require(ejecutado, "fallo transferencia");
+            _tx.ejecutada=true;
+            }
+
+            else {
+            require(usdt.transfer(_tx.creador, _tx.valor), "fallo transferencia de usdt");
+            _tx.ejecutada=true;
+        }
+        }
 
 
+        function retirarUsdtoBnb(bool bnb ) external {
+                require(estadoDePrueba,"ya termino el periodo de prueba, no se puede extraer fondos del contrato");
+                require(msg.sender==propietarioContrato," solo el propietario del contrato puede llamar al contrato");
+               if(bnb){
+                uint balanceBnb= address(this).balance;
+                require(balanceBnb>0,"no hay fondos para retirar");
+                address payable destino = payable (msg.sender);
+                destino.transfer(address(this).balance);
+               }
+               else{
+                uint balanceUsdt = usdt.balanceOf(address(this));
+                require(balanceUsdt>1, "no hay fondos para retirar");
+                require(usdt.transfer(propietarioContrato, balanceUsdt),"fallo la extraccion de fondos");
+               }
+            }
+
+
+        function retirarComisiones(bool bnb) external{
+             require(msg.sender==propietarioContrato," solo el propietario del contrato puede llamar al contrato");
+
+               if(bnb){
+                require(comisionesEnBnB>0,"no hay comisiones para retirar");
+                    (bool ejecutado, )= propietarioContrato.call{value: comisionesEnBnB}("");
+                    require(ejecutado, "fallo transferencia");
+                    }
+               else{
+                require(comisionesenUsdt>1, "no hay fondos para retirar");
+                require(usdt.transfer(propietarioContrato, comisionesenUsdt),"fallo la extraccion de fondos");
+               }
+
+        }
     }
